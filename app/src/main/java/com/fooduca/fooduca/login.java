2eaf0175f01d86f58c2499458e6aba1574376cb7
Miguel Yanes <00145016@uca.edu.sv>
2018-06-27 11:24:15 -0600

    Se creó una "simulación" experimental de estar cargando

---
+++ app/src/main/java/com/fooduca/fooduca/login.java
@@ -0,0 +1,30 @@
+package com.fooduca.fooduca;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ImageView cargando;
+
+        cargando = (ImageView) findViewById(R.id.cargando);
+        cargando.setImageResource(R.drawable.cargando);
+
+        int cont = 0;
+        while(cont != 1000000000){
+            cont = cont +1;
+        }
+        if (cont == 1000000000){
+            cont = 0;
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
