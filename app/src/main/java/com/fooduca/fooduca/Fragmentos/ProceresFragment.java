a246a52eee0ecc29d8a47adfdc651b1524d9dddb
Miguel Yanes <00145016@uca.edu.sv>
2018-06-28 12:26:43 -0600

    Se agregaron los logos corregidos a los restaurantes y eliminación de unas secciones de código que no se ocuparian

--- app/src/main/java/com/fooduca/fooduca/Fragmentos/ProceresFragment.java
+++ app/src/main/java/com/fooduca/fooduca/Fragmentos/ProceresFragment.java
@@ -38,8 +38,6 @@ public class ProceresFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
         view = inflater.inflate(R.layout.fragment_proceres, container, false);
 
         recycler = view.findViewById(R.id.Recyclerview_restaurantes_proceres);
@@ -52,31 +50,15 @@ public class ProceresFragment extends Fragment {
         res.add(new Restaurante("China Wok",R.drawable.logo_china_wok, "http://www.chinawok.com.sv/"));
         res.add(new Restaurante("Pollo Campero",R.drawable.logo_pollo_campero,"https://www.campero.com/sv"));
         res.add(new Restaurante("Burguer King",R.drawable.logo_burger_king, "https://www.bk.com/international"));
-        res.add(new Restaurante("McDonald",R.drawable.logo_mcdonalds,"https://www.mcdonalds.com/us/es-us.html"));
+        res.add(new Restaurante("McDonald",R.drawable.logo_mc_donald,"https://www.mcdonalds.com/us/es-us.html"));
         res.add(new Restaurante("Starbucks",R.drawable.logo_starbucks,"https://www.starbucks.com/"));
         res.add(new Restaurante("Wendy's",R.drawable.logo_wendys,"https://www.wendyselsalvador.com/"));
-//        res.add(new Restaurante("Pizza Hut",R.drawable.logo_pizza_hut));
-//        res.add(new Restaurante("Los Cebollines",R.drawable.logo_los_cebollines));
-//        res.add(new Restaurante("China Wok",R.drawable.logo_china_wok));
-//        res.add(new Restaurante("Pollo Campero",R.drawable.logo_pollo_campero));
-//        res.add(new Restaurante("Burguer King",R.drawable.logo_burger_king));
-//        res.add(new Restaurante("McDonald",R.drawable.logo_mcdonalds));
-//        res.add(new Restaurante("Starbucks",R.drawable.logo_starbucks));
-//        res.add(new Restaurante("Wendy's",R.drawable.logo_wendys));
+
         recycler.setLayoutManager(glm);
 
         rvadapter = new RecyclerViewRestaurantes(getContext(),res);
         recycler.setAdapter(rvadapter);
-        //intentando ir a pag al darle a la img
-//        imgview = view.findViewById(R.id.img_restaurante);
-//        imgview.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Uri uri = Uri.parse("https://www.pizzahut.com.sv/");
-//                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-//                startActivity(intent);
-//            }
-//        });
+
         return view;
     }
 
