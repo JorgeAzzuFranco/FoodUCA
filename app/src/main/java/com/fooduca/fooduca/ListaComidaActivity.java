a1b3881c0af16ec3c824f680935a2a2b3d3fe274
Sara Romero <00030716@uca.edu.sv>
2018-07-04 11:32:26 -0600

    Menu flotante para mostrar las redes sociales

--- app/src/main/java/com/fooduca/fooduca/ListaComidaActivity.java
+++ app/src/main/java/com/fooduca/fooduca/ListaComidaActivity.java
@@ -6,12 +6,14 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -32,6 +34,13 @@ public class ListaComidaActivity extends AppCompatActivity {
     RecyclerViewComida comidaAdapter;
     ImageView btn_fb;
     ImageView btn_ig;
+
+    //instancias fab menu
+    static boolean isFabOpen = false;
+    FloatingActionButton fabMain;
+    FloatingActionButton fabFb;
+    FloatingActionButton fabIg;
+    View bgFabMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,5 +89,82 @@ public class ListaComidaActivity extends AppCompatActivity {
         comidaAdapter = new RecyclerViewComida(getApplicationContext(), comidas);
         rv.setLayoutManager(glm);
         rv.setAdapter(comidaAdapter);
+
+        //Floating menu
+        fabFb = findViewById(R.id.fab_fb);
+        bgFabMenu = findViewById(R.id.bg_fab_menu);
+        fabMain = findViewById(R.id.fab_main);
+        fabIg = findViewById(R.id.fab_ig);
+
+        fabMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isFabOpen){
+                    ShowFabMenu();
+                }else {
+                    CloseFabMenu();
+                }
+            }
+        });
+        fabIg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CloseFabMenu();
+                String obtenerig = datos.getString("ig");
+                if (obtenerig != null) {
+                    Uri uri = Uri.parse(obtenerig);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(), "Este restaurante no posee ig",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+        fabFb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CloseFabMenu();
+                String obtenerfb = datos.getString("fb");
+                if (obtenerfb != null) {
+                    Uri uri = Uri.parse(obtenerfb);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(), "Este Restaurante no posee fb",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        bgFabMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CloseFabMenu();
+            }
+        });
     }
+
+    private void CloseFabMenu() {
+        isFabOpen=false;
+        fabFb.setVisibility(View.INVISIBLE);
+        fabIg.setVisibility(View.INVISIBLE);
+        bgFabMenu.setVisibility(View.INVISIBLE);
+
+        fabMain.animate().rotation(0f);
+        bgFabMenu.animate().alpha(0f);
+        fabFb.animate().translationY(0f).rotation(90f);
+        fabIg.animate().translationY(0f).rotation(90f);
+    }
+
+    private void ShowFabMenu() {
+        isFabOpen=true;
+        fabFb.setVisibility(View.VISIBLE);
+        fabIg.setVisibility(View.VISIBLE);
+        bgFabMenu.setVisibility(View.VISIBLE);
+
+        fabMain.animate().rotation(135f);
+        bgFabMenu.animate().alpha(1f);
+        fabFb.animate().translationY(-160f).rotation(0f);
+        fabIg.animate().translationY(-300f).rotation(0f);
+    }
+
 }
