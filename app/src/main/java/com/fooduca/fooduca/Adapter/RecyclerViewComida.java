85056703cc325fea15309a7275253fa11c191a48
Sara Romero <00030716@uca.edu.sv>
2018-07-07 21:51:14 -0600

    Parte del menú delicias do brasil.

--- app/src/main/java/com/fooduca/fooduca/Adapter/RecyclerViewComida.java
+++ app/src/main/java/com/fooduca/fooduca/Adapter/RecyclerViewComida.java
@@ -53,7 +53,11 @@ public class RecyclerViewComida extends RecyclerView.Adapter<RecyclerViewComida.
         if(comidas != null){
             Comida current = comidas.get(position);
             holder.Nombre_comida.setText(comidas.get(position).getNombre_comida());
-            holder.Precio.setText(String.format("$%.2f", comidas.get(position).getPrecio()));
+            if(comidas.get(position).getPrecio()==0){
+                holder.Precio.setText("$X.XX");
+            }else{
+                holder.Precio.setText(String.format("$%.2f", comidas.get(position).getPrecio()));
+            }
             holder.ComidaImg.setImageResource(comidas.get(position).getComidaImg());
             //holder.Nombre_restaurante.setText(comidas.get(position).getNombre_restaurante());
         }
